c5af66a4ca2d07cbab7964d109220fd63448724b
ryanspeciale <difficultoctet@gmail.com>
2022-06-19 08:29:36 -0400

    styled the about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,8 +10,10 @@ type aboutProps = {};
 const about = (props: aboutProps) => {
 	return (
 		<div className={styles.mainContainer}>
-			<Box justify='center' align='center' alignSelf='center' alignContent='center'>
-				<h3></h3>
+			<Box justify='center' direction='row' align='center' alignSelf='center' alignContent='center' basis='50%'>
+				<Box justify='center' align='center' alignSelf='center' alignContent='center' gap='large'>
+					<h2 className='align-middle pt-16 bg-slate-100 h-96 text-2xl font-semibold border-double border-stone-600 border-8 font-serif  mt-4 p-2 rounded-lg self-center justify-center items-center w-3/4 text-center '>The Cleaner Crew is pleased to provide families with customized cleanings organized specifically for each home. We offer a variety of different types of cleanings to target the type of service to best suit the client. In the sections below, each cleaning is broken down to explain what services are included in each cleaning; please remember, all cleanings are customizable- you name it, WE CLEAN IT!</h2>
+				</Box>
 			</Box>
 		</div>
 	);
