fa78d3d5e8735d6c58fe986ddf7a178e73c9d4cc
ryanspeciale <difficultoctet@gmail.com>
2022-07-30 10:53:02 -0400

    new api routes

---
+++ pages/api/delete.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { withSessionRoute } from '../../lib/withSession'
+import { prisma } from "../../lib/prisma";
+
+export default withSessionRoute(deleteReview)
+
+
+export async function deleteReview(req: NextApiRequest, res: NextApiResponse) {
+    const { id } = req.body
+    const user = req.session.user;
+    if (!user) {
+        res.json({ Message: "Bad Request. You are not allowed to do this"})
+    }
+    try {
+        await prisma.review.delete({
+            where: {
+                id: id
+            }
+        })
+    } catch(error) {
+        res.json({ Message: error })
+    }
+    
+}
