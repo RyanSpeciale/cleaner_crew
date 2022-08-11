83716a6e8d7dec93640e5bda338333dadbf75f23
ryanspeciale <difficultoctet@gmail.com>
2022-08-11 18:17:40 -0400

    last change

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,7 +8,7 @@ import { Typography } from '@mui/material';
 
 type aboutProps = {};
 
-const message: string = 'The Cleaner Crew is pleased to provide families with customized cleanings organized specifically for each home. We offer a variety of different types of cleanings to target the type of service to best suit the client. In the sections below, each cleaning is broken down to explain what services are included in each cleaning; please remember, all cleanings are customizable- you name it, WE CLEAN IT!'
+const message: string = 'The Cleaner Crew is pleased to provide families with customized cleanings organized specifically for each home. We offer a variety of different types of cleanings to target the type of service to best suit the client. In the services tabs, each cleaning is broken down to explain what services are included in each cleaning; please remember, all cleanings are customizable- you name it, WE CLEAN IT!'
 
 const about = (props: aboutProps) => {
 	return (
